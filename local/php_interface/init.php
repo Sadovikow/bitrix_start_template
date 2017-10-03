634687e4034881af916650c5253d3005475bbaf3
Sergey <nexxerr@gmail.com>
2017-10-03 13:39:25 +0300

    Create init.php

---
+++ local/php_interface/init.php
@@ -0,0 +1,9 @@
+<?
+if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/js/include.php")){
+    require_once($_SERVER["DOCUMENT_ROOT"]."/local/js/include.php");
+}
+
+if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/functions.php")){
+    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/functions.php");
+}
+?>
