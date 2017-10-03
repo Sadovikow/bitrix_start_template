eba6e56ebe4b6cecd0deb3fa1ef7e6d1e7b2a19f
Sergey <nexxerr@gmail.com>
2017-10-03 13:40:38 +0300

    Create iblock.php

---
+++ local/php_interface/include/iblock.php
@@ -0,0 +1,7 @@
+<?
+define("IBLOCK_CATALOG_ID", 7); // Основной каталог товаров
+define("IBLOCK_SPECIFICATIONS_ID", 8); //Технические характеристики
+define("IBLOCK_FEEDBACKS_ID", 32);  // Отзывы
+define("IBLOCK_LEARNING_ID", 36);  // Обучение
+define("IBLOCK_DISTRIBUTOR_ID", 37);  // Дистрибьюторы
+?>
