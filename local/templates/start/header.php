9dc30d329f38e7b0fd2e4e0daf5900d61e92aea2
Sergey <nexxerr@gmail.com>
2017-11-03 17:48:23 +0300

    facico +apple-touch-icon
    16x16 32x32 96x96

--- local/templates/start/header.php
+++ local/templates/start/header.php
@@ -8,9 +8,10 @@
         use Bitrix\Main\Page\Asset;
         Asset::getInstance()->addJs(SITE_TEMPLATE_PATH.'/script.js');
         CJSCore::Init(array('scripts')); // Инициализация скриптов в /js/include.php?>
-        <link rel="icon" href="<?=SITE_TEMPLATE_PATH?>/img/favicon.ico" type="image/x-icon">
-        <link rel="shortcut icon" href="<?=SITE_TEMPLATE_PATH?>/img/favicon.ico" type="image/x-icon">
-        <link rel="apple-touch-icon-precomposed" href="<?=SITE_TEMPLATE_PATH?>/img/favicon.ico">
+    <link rel="icon" type="image/png" href="<?=SITE_TEMPLATE_PATH?>/img/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="<?=SITE_TEMPLATE_PATH?>/img/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?=SITE_TEMPLATE_PATH?>/img/favicon-96x96.png" sizes="96x96">
+    <link rel="apple-touch-icon-precomposed" href="<?=SITE_TEMPLATE_PATH?>/img/favicon-96x96.png">
         <meta name=viewport content="width=device-width, initial-scale=1">
         <meta name="format-detection" content="telephone=no">
     </head>
