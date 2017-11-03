131d054dd5b3b1cc83eb4cc5d23f6532b1f94a28
Sergey <nexxerr@gmail.com>
2017-11-03 10:39:00 +0300

    Update footer.php

--- local/templates/start/footer.php
+++ local/templates/start/footer.php
@@ -1,9 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?IncludeTemplateLangFile(__FILE__);?>
-</div>
-<footer>
-</footer>
-
-</body>
+    </div>
+  </body>
 </html>
-<!— Содержание футера— —>
+
