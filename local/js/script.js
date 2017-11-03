2e97c93a644e34d96492c7cf42c0d43d3a7080ed
Sergey <nexxerr@gmail.com>
2017-11-03 10:48:13 +0300

    Input masks for tel

--- local/js/script.js
+++ local/js/script.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
+/* input masks */
+    $("input[type='tel']").inputmask("+7(999)999-99-99",{
+        oncomplete: function(){ },
+        onincomplete: function(){ $(this).val(""); },
+        oncleared: function(){ $(this).val(""); },
+    });
+/* input masks end */
 
+});
