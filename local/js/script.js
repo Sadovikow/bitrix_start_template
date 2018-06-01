e31dc422ac68a06e9c12949480d64bee4ea4a35b
Sergey Sadovikov <nexxerr@gmail.com>
2018-06-01 16:06:29 +0300

    Update script.js

--- local/js/script.js
+++ local/js/script.js
@@ -5,6 +5,31 @@ $(document).ready(function() {
         onincomplete: function(){ $(this).val(""); },
         oncleared: function(){ $(this).val(""); },
     });
+    $("input.tel").inputmask("+7(999)999-99-99",{
+        oncomplete: function(){ },
+        onincomplete: function(){ $(this).val(""); },
+        oncleared: function(){ $(this).val(""); },
+    });
+    $("input.date").inputmask("99.99.9999",{
+        oncomplete: function(){ },
+        onincomplete: function(){ $(this).val(""); },
+        oncleared: function(){ $(this).val(""); },
+    });
+    $("input.orgn").inputmask("9999999999999",{
+        oncomplete: function(){ },
+        //onincomplete: function(){ $(this).val(""); },
+        //oncleared: function(){ $(this).val(""); },
+    });
+    $("input.inn").inputmask("999999999999",{
+        oncomplete: function(){ },
+        //onincomplete: function(){ $(this).val(""); },
+        //oncleared: function(){ $(this).val(""); },
+    });
+    $("input.kpp").inputmask("999999999",{
+        oncomplete: function(){ },
+        //onincomplete: function(){ $(this).val(""); },
+        //oncleared: function(){ $(this).val(""); },
+    });
 /* input masks end */
 
 });
