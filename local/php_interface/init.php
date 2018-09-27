6288a58588db822200d06e927f031189e2606af0
Sergey Sadovikov <nexxerr@gmail.com>
2018-09-27 19:00:07 +0300

    Update init.php

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,9 +1,19 @@
-<?if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/functions.php")){
+<?
+// Functions
+if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/functions.php")){
     require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/functions.php");
 }
-if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/iblock.php")){
-    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/iblock.php");
+// Iblock constants
+if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/constants.php")){
+    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/constants.php");
 }
+
+// JavaScript libraries
 if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/js/include.php")){
     require_once($_SERVER["DOCUMENT_ROOT"]."/local/js/include.php");
-}?>
+}
+
+// Handlers
+if(file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/handlers.php")){
+    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/handlers.php");
+}  ?>
