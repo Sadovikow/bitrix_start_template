1d9728a7aa389d9a6de53198e1bcbfc6631d4e81
Sergey Sadovikov <nexxerr@gmail.com>
2018-10-18 18:35:26 +0300

    dump

--- local/php_interface/include/functions.php
+++ local/php_interface/include/functions.php
@@ -1,14 +1,12 @@
 <?
-function dump($var, $die=false, $all=false)
+function dump($var, $id=false, $all=false)
 {
       global $USER;
-      if( ($USER->GetID()==1) || ($all==true) )
+      if( ($USER->GetID()==1) || ($all==true) || $USER->GetID()==$id)
       {
             echo '<pre>';
             print_r($var);
             echo '</pre>';
       }
-      if($die)
-      die('hello');
 }
 ?>
