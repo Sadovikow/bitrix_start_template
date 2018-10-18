4c64239179ce4c6e849a1165c62b05c98ffbe4b9
Sergey Sadovikov <nexxerr@gmail.com>
2018-10-18 18:43:15 +0300

    Update functions.php

--- local/php_interface/include/functions.php
+++ local/php_interface/include/functions.php
@@ -1,4 +1,13 @@
 <?
+/**
+ * Default dump
+ *
+ * @param   $var
+ * @param   $id
+ * @param   $all
+ *
+ * @return  mixed
+ */
 function dump($var, $id=false, $all=false)
 {
       global $USER;
@@ -8,5 +17,37 @@ function dump($var, $id=false, $all=false)
             print_r($var);
             echo '</pre>';
       }
+}
+
+/**
+ * Преобразование количества "цифрой" в количество "словом"
+ * Чтобы было удобнее формировать массивы со склонениями, запомните ряд чисел 1-2-5,
+ * а потом мысленно подставляйте их в массив: (один "рубль", два "рубля", пять "рублей")
+ *     $num = 3;
+ *     $words = array('новость', 'новости', 'новостей');
+ *     echo $num . '  ' . num2word($num, $words); // сколько новостей
+ *
+ * @param   $n
+ * @param   $words
+ *
+ * @return  mixed
+ */
+function num2word($num, $words)
+{
+    $num = $num % 100;
+    if ($num > 19) {
+        $num = $num % 10;
+    }
+    switch ($num) {
+        case 1: {
+            return($words[0]);
+        }
+        case 2: case 3: case 4: {
+            return($words[1]);
+        }
+        default: {
+            return($words[2]);
+        }
+    }
 }
 ?>
