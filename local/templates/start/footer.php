f2f624ecdda9032324a097c8cdc81acae09d433c
Sergey Sadovikov <nexxerr@gmail.com>
2019-11-16 16:02:24 +0300

    footer microdate

--- local/templates/start/footer.php
+++ local/templates/start/footer.php
@@ -1,6 +1,17 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?IncludeTemplateLangFile(__FILE__);?>
     </div>
+    <div itemscope itemtype="https://schema.org/Organization" style="display: none;">
+        <span itemprop="name">Your organization</span>
+        <div itemprop="address" itemscope itemtype="https://schema.org/PostalAddress">
+            <span itemprop="streetAddress">ул. Большая Академическая, д. 5, стр 1</span>,
+            <span itemprop="addressLocality">Москва</span>.
+        </div>
+        <img itemprop="logo" src="/logo.png" />
+        <meta itemprop="telephone" content="+7 (495) 150-27-48" />
+        <a itemprop="url" href="https://www.yoursite.ru/"></a>
+        <meta itemprop="email" content="info@yourorganization.ru">
+    </div>
   </body>
 </html>
 
